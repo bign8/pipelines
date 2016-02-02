ef88e2a725c319a4629524e647745f2fe6815a73
Nate Woods <big.nate.w@gmail.com>
2016-02-02 01:01:56 -0700

    Adding partial bootstrap function

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# CONFIGURE GIT
+# TODO: install git
+git config --global color.ui true
+git config --global user.name "Nate Woods"
+git config --global user.email big.nate.w@gmail.com
+
+# Clone Repository
+git clone https://bign8@bitbucket.org/bign8/pipelines.git
+cd pipelines
+
+# Setup console
+echo "source ~/pipelines/config/dot.profile" > ~/.profile
+source ~/.profile
+
