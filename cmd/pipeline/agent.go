ed27b3487e0fafca924fac394cf937e1b203b181
Nate Woods <big.nate.w@gmail.com>
2016-02-15 08:00:26 -0700

    Getting agent up and running

--- cmd/pipeline/agent.go
+++ cmd/pipeline/agent.go
@@ -27,47 +27,69 @@ func runAgent(cmd *Command, args []string) {
 		panic(err)
 	}
 
-	// Get IP address for logging purposes
-	var IPs []string
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		panic(err)
-	}
-	// handle err
-	for _, i := range ifaces {
-		addrs, err := i.Addrs()
-		if err != nil {
-			panic(err)
-		}
-		// handle err
-		for _, addr := range addrs {
-			var ip net.IP
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
-			}
-			// process IP address
-			if ip != nil && !ip.IsLoopback() {
-				IPs = append(IPs, ip.String())
-			}
-		}
+	<-NewAgent(nc).Done
+}
+
+// Agent is the base type for an agent
+type Agent struct {
+	Done chan struct{}
+}
+
+// NewAgent constructs a new agent... duh!!!
+func NewAgent(nc *nats.Conn) *Agent {
+	agent := &Agent{
+		Done: make(chan struct{}),
 	}
 
 	// Find diling address for agent to listen to; TODO: make this suck less
 	var msg *nats.Msg
-	err = errors.New("starting")
-	addrs := strings.Join(IPs, ",")
+	err := errors.New("starting")
+	details := []byte(getIPAddrDebugString())
 	for err != nil {
-		msg, err = nc.Request("pipelines.server.agent.start", []byte(addrs), 100*time.Millisecond)
+		msg, err = nc.Request("pipelines.server.agent.start", details, time.Second)
 	}
+	log.Printf("Assigned UUID: %s", msg.Data)
 
-	nc.Subscribe("pipelines.agent."+string(msg.Data), func(m *nats.Msg) {
-		log.Printf("Dealing with msg: %+v", m)
-	})
-	log.Printf("Dealing with msg: %s", msg.Data)
+	nc.Subscribe("pipelines.agent."+string(msg.Data), agent.handleAgent)
 
-	// log.Printf("Sending data: %v", args)
-	// nc.Publish(args[0], []byte(args[1]))
+	return agent
+}
+
+func (a *Agent) handleAgent(m *nats.Msg) {
+	log.Printf("Dealing with AGENT msg: %+v", m)
+}
+
+func getIPAddrDebugString() string {
+	// Get IP address for logging purposes; TODO: make this suck less
+	unique := make(map[string]net.IP)
+	var IPs []string
+	ifaces, err := net.Interfaces() // net.InterfaceByName("en0") //
+	if err != nil {
+		panic(err)
+	}
+	for _, i := range ifaces {
+		if strings.HasPrefix(i.Name, "en") {
+			addrs, err := i.Addrs()
+			if err != nil {
+				continue
+			}
+			for _, addr := range addrs {
+				var ip net.IP
+				switch v := addr.(type) {
+				case *net.IPNet:
+					ip = v.IP
+				case *net.IPAddr:
+					ip = v.IP
+				}
+				// process IP address
+				ip = ip.To4()
+				if _, ok := unique[ip.String()]; ok || ip == nil {
+					continue
+				}
+				unique[ip.String()] = ip
+				IPs = append(IPs, i.Name+" "+i.HardwareAddr.String()+" "+ip.String())
+			}
+		}
+	}
+	return strings.Join(IPs, ",")
 }
