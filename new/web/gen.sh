988969c6b151bffc3733cadd5ca914bf06e0a7f4
Nate Woods <big.nate.w@gmail.com>
2016-10-15 22:19:00 -0600

    Adding certs to the container
    Not neccessary at this time, but something to be done

--- new/web/gen.sh
+++ new/web/gen.sh
@@ -1,17 +1,26 @@
 #! /bin/bash
+# Thanks: https://blog.codeship.com/building-minimal-docker-containers-for-go-applications/
 
-read -r -d '' DOCKERFILE <<- EOM
+set -e # causes the whole script to fail if any commands fail
+set -o pipefail # causes pipes to fail if the input command fails
+
+cat > Dockerfile <<- EOM
 	FROM scratch
+	ADD ca-certificates.crt /etc/ssl/certs/
 	ADD main /
 	CMD ["/main"]
 EOM
 
-# echo "$DOCKERFILE" > Dockerfile
+echo -n "Downloading CA Certs "
+curl https://curl.haxx.se/ca/cacert.pem -# -o certz.crt 2>&1 | xxd | while read; do echo -n .; done
+echo " Done"
 
 # find go source directories (TODO: assert their package is main)
 find */* -name "*.go" -print0 | xargs -0 -n1 dirname | sort -u | while read line; do
   echo "Processing Directory '$line'"
   pushd $line > /dev/null
+  cp ../certz.crt ca-certificates.crt
+  cp ../Dockerfile Dockerfile
 
   # Generate go source
   echo -en "\tGolang "
@@ -20,14 +29,17 @@ find */* -name "*.go" -print0 | xargs -0 -n1 dirname | sort -u | while read line
 
   # Generating docker container
   echo -en "\tDocker "
-  echo "$DOCKERFILE" > Dockerfile
   docker build -t pipeline-web-"$line":latest . | while read; do echo -n .; done
   echo " Done"
 
   # Cleaning up assets
   echo -en "\tFinish "
-  rm -v main Dockerfile | while read; do echo -n .; done
+  rm -v main Dockerfile ca-certificates.crt | while read; do echo -n .; done
   echo " Done"
 
   popd > /dev/null
 done
+
+# Cleanup files
+rm certz.crt Dockerfile
+echo "Complete"
