be1f68e417a2d401e367ba5e7ddae9aaaa237d83
Nate Woods <big.nate.w@gmail.com>
2016-10-16 11:21:38 -0600

    Starting to connect to NATS

--- new/pipelines.go
+++ new/pipelines.go
@@ -1,10 +1,53 @@
 package pipelines
 
-import "io"
+import (
+	"io"
+	"os"
+	"sync"
+	"time"
 
+	"github.com/nats-io/nats"
+)
+
+// Type assertions for internal types
 var (
 	_ Unit = (*unit)(nil)
 )
+
+// Envirment defined variables
+var (
+	NATS_ADDR = getEnv("NATS_ADDR", nats.DefaultURL)
+	LINE_PREF = getEnv("PL_PREFIX", "pipelines")
+	EMIT_CHAN = LINE_PREF + ".emit"  // data is emitted to admin cluster with this
+	EMIT_POOL = "emit"               // pool used among admins to rotate through emit requests
+	MINE_PREF = LINE_PREF + ".mine." // Miners add the following suffix: "<stream>.<name>"
+	MINE_POOL = "mine"               // pool used by clients to rotate through mine requests
+	NAME_CHAN = LINE_PREF + ".name"  // called to register new consumers with admin
+	NAME_POOL = "pool"               // pool used by admins to rotate through name requests
+)
+
+// Internal NATS connection (per server)
+var (
+	conn *nats.Conn
+	lock sync.Mutex
+)
+
+func getEnv(key, def string) string {
+	out := os.Getenv(key)
+	if out == "" {
+		return def
+	}
+	return out
+}
+
+func connect() (err error) {
+	lock.Lock()
+	defer lock.Unlock()
+	if conn == nil {
+		conn, err = nats.Connect(NATS_ADDR)
+	}
+	return err
+}
 
 // Worker is a consumer defined object that can do work
 type Worker interface {
@@ -72,7 +115,7 @@ type Generator func(Stream) Worker
 
 // sub is an internal subscription for a worker that consumers can close
 type computation struct {
-	comp Worker
+	conf Config
 	done chan struct{}
 	// All the awesome connector stuff
 }
@@ -80,7 +123,7 @@ type computation struct {
 // Register starts a worker and provides a closer
 func Register(config Config) (io.Closer, error) {
 	proc := &computation{
-		// comp: comp,
+		conf: config,
 		done: make(chan struct{}),
 	}
 	go proc.run()
@@ -95,8 +138,33 @@ func (c *computation) Close() error {
 	return nil
 }
 
+func runMiner(name string, stream Stream, ext Extractor) (*nats.Subscription, error) {
+	return conn.QueueSubscribe(MINE_PREF+string(stream)+"."+name, MINE_POOL, func(msg *nats.Msg) {
+		// TODO (bign8): convert payload to Unit the correct way
+		unit := NewUnit("TODO", msg.Data)
+		key := ext(unit)
+		conn.Publish(msg.Reply, []byte(key))
+	})
+}
+
 // run is the main process loop for a computation
 func (c *computation) run() {
+
+	// TODO: send this through batching sender
+	msg, err := conn.Request(NAME_CHAN, []byte(c.conf.Name), 50*time.Millisecond)
+	if err != nil || string(msg.Data) != "ACK" {
+		return
+	}
+
+	// Startup miners process
+	for stream, miner := range c.conf.Inputs {
+		sub, err := runMiner(c.conf.Name, stream, miner)
+		if err != nil {
+			return
+		}
+		defer sub.Unsubscribe()
+	}
+
 	// TODO: initialize comz based on c.comp.Config()
 	for {
 		select {
@@ -107,8 +175,16 @@ func (c *computation) run() {
 }
 
 // Emit broadcasts a record into the system
+// TODO (bign8): add to batcher/timeout queue
 func Emit(stream Stream, obj Unit) error {
-	// add retry queue
+	if err := connect(); err != nil {
+		return err
+	}
+	msg, err := conn.Request(EMIT_CHAN, obj.Load(), 50*time.Millisecond)
+	if err != nil || string(msg.Data) != "ACK" {
+		// TODO (bign8): retry to send to admin via jittered backoff https://www.awsarchitectureblog.com/2015/03/backoff.html
+		return err
+	}
 	return nil
 }
 
